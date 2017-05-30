1a08a5ecdb95f140a77be38f77901e29cf6fb829
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2017-05-30 15:36:22 +0300

    Empty solution with 2 projects

---
+++ Dodo.LegacyCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dodo.LegacyCode
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+              Console.WriteLine("Hello. We've just started.");   
+        }
+    }
+}
