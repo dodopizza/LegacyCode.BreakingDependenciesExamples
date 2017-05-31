c133f3093dea8b1c7e0fb0967f6603ec992faa10
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2017-05-31 16:53:26 +0300

    preserve signatures initial state

---
+++ Dodo.LegacyCode/0-PreserveSignatures/OrderCalculator.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Dodo.LegacyCode
+{
+    public class OrderCalculator
+    {
+        public void Process(List<Order> orders,
+                            int dailyTarget,
+                            double interestRate,
+                            int compensationPercent)
+        {
+            // some complicated logic goes here ...
+        }
+    }
+}
