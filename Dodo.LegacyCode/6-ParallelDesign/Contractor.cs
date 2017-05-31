efdd86a0f8c14b70530354b16e564660708ce983
Dmitry Pavlov <dmitry.pavlov@smartstepgroup.com>
2017-05-31 17:12:24 +0300

    Parallel Design and Static class

---
+++ Dodo.LegacyCode/6-ParallelDesign/Contractor.cs
@@ -0,0 +1,7 @@
+namespace Dodo.LegacyCode
+{
+    public class Contractor
+    {
+        // some old legacy class with messy logic ...
+    }
+}
---
+++ Dodo.LegacyCode/7-StaticClass/Utils.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Dodo.LegacyCode
+{
+    class Presenter
+    {
+        public void Show()
+        {
+            // ...
+
+            var formattedDateTime = Utils.Format(DateTime.UtcNow);
+
+            // ...
+        }
+    }
+
+    public static class Utils
+    {
+        public static string Format(DateTime dateTime)
+        {
+            return $"DateTime - {dateTime.ToLongTimeString()}";
+        }
+
+        public static void ReFormat()
+        {
+            // ...
+        }
+
+        public static void AnotherFormat()
+        {
+            // ...
+        }
+    }
+}
